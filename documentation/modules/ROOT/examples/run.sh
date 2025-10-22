6f5e143695d3e9f21ba5dd4e7082aeb57a84624f
Diego Garcia <dgpmakes@gmail.com>
2025-10-22 11:46:14 +0200

    :hammer AWS CLI Installation check

--- documentation/modules/ROOT/examples/run.sh
+++ documentation/modules/ROOT/examples/run.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-echo "Hello World"
+# Check AWS CLI is installed
+which aws
+
+# If not installed, install it
+if [ $? -ne 0 ]; then
+    echo "AWS CLI is not installed, installing it..."
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+fi
+
+echo "AWS CLI is installed!"
+
+# Verify installation
+aws --version
